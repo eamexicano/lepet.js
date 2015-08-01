b685bb9fe7e2e7b7cbc3c8682ffc4eed0735938a
eamexicano <emmanuelayalamexicano@gmail.com>
2015-07-31 20:25:21 -0500

    Add an explicit file for content, to make it easier to implement - instead of adding content in the same lepet file -.

---
+++ js/content.js
@@ -0,0 +1,27 @@
+var content = [
+  {
+    locale: 'es',
+    doclanguage: 'es-MX',
+    title: "Lepet",
+    document_title: "Título",
+    main: "Contenido Principal. ",
+    secondary: "<span class='custom'>Contenido Secundario.</span>"
+  },
+  {
+    locale: 'ru',
+    doclanguage: 'ru-RU',
+    title: "лепет",
+    document_title: "Название",
+    main: "основное содержание.",
+    secondary: "<span class='custom'>второй содержание.</span>"
+  },
+  {
+    locale: 'fr',
+    doclanguage: 'fr-FR',
+    title: "Lepet",
+    document_title: "Titre",
+    main: "Contenu principal.",
+    secondary: "<span class='custom'>Contenu secondaire.</span>"
+  }
+  
+];
