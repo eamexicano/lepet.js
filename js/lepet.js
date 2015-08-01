c49bb60610d8456fc0f9d1267f95cd984bb6b214
eamexicano <emmanuelayalamexicano@gmail.com>
2015-07-31 20:08:56 -0500

    Update lepet content and add french locale to test dynamic link generation

--- js/lepet.js
+++ js/lepet.js
@@ -28,17 +28,28 @@ var content = [
   {
     locale: 'es',
     doclanguage: 'es-MX',
-    title: "Título",
-    main: "Contenido Principal",
+    title: "Lepet",
+    document_title: "Título",
+    main: "Contenido Principal. ",
     secondary: "<span class='custom'>Contenido Secundario.</span>"
   },
   {
     locale: 'ru',
     doclanguage: 'ru-RU',
-    title: "Название",
-    main: "основное содержание",
+    title: "лепет",
+    document_title: "Название",
+    main: "основное содержание.",
     secondary: "<span class='custom'>второй содержание.</span>"
+  },
+  {
+    locale: 'fr',
+    doclanguage: 'fr-FR',
+    title: "Lepet",
+    document_title: "Titre",
+    main: "Contenu principal.",
+    secondary: "<span class='custom'>Contenu secondaire.</span>"
   }
+  
 ];
 
 
