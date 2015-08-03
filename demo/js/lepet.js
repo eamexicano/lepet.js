1835792718ccedcbb77a17782a25e881d508eba4
eamexicano <emmanuelayalamexicano@gmail.com>
2015-08-03 13:46:04 -0500

    Use the main version of lepet for the demo

--- demo/js/lepet.js
+++ demo/js/lepet.js
@@ -32,27 +32,19 @@ THE SOFTWARE.
     var available_locales = document.getElementsByClassName('locale');
     var lepet = document.getElementById('lepet');
     var locale_links = ""; 
+    var default_dir = "ltr";
      
-    /* 
-      Guardar las claves del objeto "content" 
-    */
-
-      for (var i = 0; i < content.length; i++) { 
-        keys.push(Object.keys(content[i]));
-        locale_links += "<a href='#" + content[i].locale + "' class='locale'>" + content[i].locale + "</a>";        
-      }
+        for (var i = 0; i < content.length; i++) { 
+          keys.push(Object.keys(content[i]));
+          locale_links += "<a href='#" + content[i].locale + "' class='locale'>" + content[i].locale + "</a>";        
+        }
   
       lepet.innerHTML = locale_links;
-      
-    /*
-      Con las claves, intentar obtener los nodos del dom por id, class o tag 
-      para que sean  actualizados al momento de cambiar de idioma. 
-    */
-      
+            
       for (var i = 0; i < keys.length; i++) {
         for (var j = 0; j < keys[i].length; j++) {
           var dom_node = keys[i][j].toString();
-          if (dom_node !== 'locale' && dom_node !== 'doclanguage') {
+          if (dom_node !== 'locale' && dom_node !== 'doclanguage' && dom_node !== 'default_dir') {
             if (document.getElementById(dom_node) !== null && !lepet_nodes.hasOwnProperty(dom_node)) {
               lepet_nodes[dom_node] = document.getElementById(dom_node);
             } else if (document.getElementsByClassName(dom_node).length > 0 && !lepet_nodes.hasOwnProperty(dom_node)) {
@@ -64,23 +56,21 @@ THE SOFTWARE.
         }
       }
 
-    /*
-        Actualiza el contenido del documento con las traducciones.
-        Para el contenido se utiliza innerHTML para permitir el uso e interpretación 
-        de HTML dentro de las traducciones. 
-    */
     function update_content(href) {
       var current_translation = find_locale(href);      
-      /*
-          Asignar a los nodos para traducir el contenido de la traducción. 
-      */
-      
+
       if (current_translation.hasOwnProperty('doclanguage')) {
         main_document.lang = current_translation.doclanguage;
       } else {
         main_document.lang = current_translation.locale;
       }
       
+      if (current_translation.hasOwnProperty('dir')) {
+        main_document.dir = current_translation.dir;
+      } else {
+        main_document.dir = default_dir;
+      }
+                  
      var dom_nodes = Object.keys(lepet_nodes);
      
      for (var i = 0; i < dom_nodes.length; i++) {
@@ -92,10 +82,7 @@ THE SOFTWARE.
      }
      
     }
-    /*
-        Busca las traducciones con base en el código del idioma provisto. 
-        Si el código no existe se devuelven las primeras traducciones.
-    */
+
     function find_locale(locale) {
       for (var i = 0; i < content.length; i++) {
         if (content[i].locale === locale) {
@@ -105,10 +92,6 @@ THE SOFTWARE.
       return content[0];
     }
     
-    /*
-        Agregar la clase active al idioma que se está visualizando.
-        Se tiene que crear en CSS
-    */
     function highlight_current_locale(all, current) {
         for (var i = 0; i < all.length; i++) {
             all[i].className = 'locale';
@@ -116,9 +99,6 @@ THE SOFTWARE.
         current.className = 'locale active';
     }
     
-    /*    
-        Utilizar attach event para tener compatibilidad con Oldie.
-    */
     function attach_link(current_locale) {
         var href = current_locale.href.slice(-2);
         if (window.addEventListener) {
@@ -133,9 +113,7 @@ THE SOFTWARE.
             });
         }
     }
-    /*
-        El ciclo se va a ejecutar al momento de ejecución porque está dentro de una IIFE.
-    */
+
     for (var i = 0; i < available_locales.length; i++) {
         attach_link(available_locales[i]);
     }
